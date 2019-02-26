612bb25558461a507f2cf2e58618a979e12d49ad
BRIUG <michaelyangbo@outlook.com>
2019-02-27 02:01:19 +0800

    Basic tools.

---
+++ src/utils.go
@@ -0,0 +1,23 @@
+package src
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+func IntToByte(num int64) []byte {
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, num)
+	CheckErr("IntToByte", err)
+	return buffer.Bytes()
+}
+
+func CheckErr(pos string, err error) {
+	if err != nil {
+		fmt.Printf("error , pos :", pos, err)
+		os.Exit(1)
+	}
+
+}
