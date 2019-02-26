9a0a43e351154ba6b914d7b57617f19be5f38d68
BRIUG <michaelyangbo@outlook.com>
2019-02-27 02:02:55 +0800

    Main function.

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package src
+
+/*
+ Name: Michael Young
+ Email: michaelyangbo@outlook.com
+ Date: 2019/2/27
+ Ref.: https://en.wikipedia.org/wiki/Blockchain
+*/
+
+func main() {
+	cli := CLI{}
+	cli.Run()
+}
